a5adbbc600ce93ffdb3dc057566e3c3fc8526a2d
Christopher Stanley <cbstanley12@gmail.com>
2019-04-21 10:57:27 -0600

    Separated css and js; Smoothed transitions

---
+++ index.js
@@ -0,0 +1,31 @@
+const secondHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function checkPosition(ele, deg) {
+    if(deg === 90) {
+        ele.style.transition = 'all 0.0s';
+    }
+}
+
+function setDate() {
+    const now = new Date();
+
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + 90;
+    minHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+    const hours = now.getHours();
+    const hoursDegrees = ((hours / 12) * 360) + 90;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+    checkPosition(secondHand, secondsDegrees);
+    checkPosition(minHand, minsDegrees);
+    checkPosition(hourHand, hoursDegrees);
+}
+
+setInterval(setDate, 1000);
